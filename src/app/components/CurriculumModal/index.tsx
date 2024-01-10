3d4a1d27fec6eefeb200986735062d3ac8372481
diegorezm <dmourarezende@gmail.com>
2024-01-10 13:47:03 -0300

    styling the modal component

---
+++ src/app/components/CurriculumModal/index.tsx
@@ -0,0 +1,33 @@
+import ModalContainer from '../ModalContainer'
+import Special from '../Special'
+import './modal.css'
+import { useEffect } from 'react'
+interface props {
+  toggleModal: () => void
+}
+export default function CurriculumModal({ toggleModal }: props) {
+  useEffect(() => {
+    document.body.classList.add('modal__open');
+    return () => {
+      document.body.classList.remove('modal__open');
+    };
+  }, [])
+  return (
+    <ModalContainer>
+      <div className="modal__dialog">
+        <div className='modal__title'>
+          Curriculum
+        </div>
+        <div className="separator"></div>
+        <div className="modal__body">
+          <Special>Would you like to download my curriculum?</Special>
+        </div>
+        <div className="separator"></div>
+        <div className="modal__buttons">
+          <a className="modal__anchor anchor__ok" href="https://raw.githubusercontent.com/diegorezm/portifolio/main/src/assets/curriculum.pdf" rel="noopener noreferrer">Ok</a>
+          <a className="modal__anchor anchor__cancel" onClick={toggleModal}>Cancel</a>
+        </div>
+      </div>
+    </ModalContainer>
+  )
+}
