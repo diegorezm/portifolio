6ce034ffff3613e142274dd47e994bfde92be732
diegorezm <dmourarezende@gmail.com>
2024-02-05 17:36:47 -0300

    dropinAnimation

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -31,5 +31,26 @@ export const goTo = (id: string) => {
 export const defaultButtonAnimation = {
   whileHover: { scale: 1.06 },
   whileTap: { scale: 0.9 }
+}
+
+export const dropInAnimation = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
 
 }
