74379c92e65126aecbc6b085f227b3dee81b5222
diegorezm <dmourarezende@gmail.com>
2024-02-05 17:36:53 -0300

    adding details button

--- src/app/components/projects/index.tsx
+++ src/app/components/projects/index.tsx
@@ -11,9 +11,20 @@ import { useEffect, useState } from "react"
 import { ProjectsInterface } from "@/app/interfaces"
 import { motion } from "framer-motion"
 import { defaultButtonAnimation } from "@/lib"
+import DetailsModal from "../DetailsModal"
 
 export default function Projects() {
   const [projects, setProjects] = useState<ProjectsInterface[]>([])
+  const [details, setDetails] = useState<ProjectsInterface>({
+      id:-1,
+      name:"",
+      showcase:"",
+      github:"",
+      image:"",
+      tech:[]
+      })
+  const [detailsModal, setDetailsModal] = useState(false)
+
   useEffect(() => {
     loadProjectsData().then(response => setProjects(response))
   }, [])
@@ -21,50 +32,36 @@ export default function Projects() {
   function imageLoader({ src }: { src: string }) {
     return `https://raw.githubusercontent.com/diegorezm/portifolio/assets/src/assets/proj/${src}`
   }
+
+  function toggleModal(){
+    setDetailsModal(!detailsModal)
+  }
+
   return (
     <Container primary={false}>
+      {detailsModal && <DetailsModal project={details} toggleModal={toggleModal}/>}
       <div id="projects" />
       <section className="projects__wrapper">
         <Title title="Projects" />
         < Subtitle message="Apps i made with the purpose of studying programming!" />
         <ul className="projects__list">
           {
-            projects.map(({ id, name, github, tech, image, showcase }) => (
+            projects.map(({ id, name, tech, image, github, showcase }) => (
               <li key={id} className="list__item__wrapper">
                 <h1>{name}</h1>
                 <div className="list__image__wrapper">
                   <Image loader={imageLoader} src={image} alt="Image project" className="list__image" width={100} height={100} quality={80} />
                 </div>
-                <div className="list__anchor__action">
-                  <motion.a
-                    {...defaultButtonAnimation}
-                    href={github}
-                    target="_blank"
-                  >
-                    Code
-                  </motion.a>
-                  <motion.a
-                    {...defaultButtonAnimation}
-                    href={showcase}
-                    target="_blank">
-                    Showcase
-                  </motion.a>
-                </div>
-                <div className="mobile__anchor__action">
-                  <motion.a
-                    {...defaultButtonAnimation}
-                    href={github}
-                    target="_blank"
-                  >
-                    Code
-                  </motion.a>
-                  <motion.a
-                    {...defaultButtonAnimation}
-                    href={showcase}
-                    target="_blank">
-                    Showcase
-                  </motion.a>
-                </div>
+                <motion.button
+                  className="details__button"
+                  {...defaultButtonAnimation}
+                  onClick={() => {
+                    toggleModal()
+                    setDetails({ id, name, image, github, showcase, tech })
+                  }}
+                >
+                  Details
+                </motion.button>
                 <div className="list__tag">
                   {tech.map((item, index) => (
                     <Tag tech={item} key={index} />
